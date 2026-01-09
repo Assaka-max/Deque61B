cabc3ba63f108b77b7cc60af877ec1a4cd3bf950
Assaka <2682728781@qq.com>
2026-01-09 14:08:00 +0800

    finish addFirst_Last_toList

--- src/LinkedListDeque61B.java
+++ src/LinkedListDeque61B.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedListDeque61B<T> implements Deque61B<T>{
@@ -24,17 +25,33 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
 
     @Override
     public void addFirst(T x) {
-
+        Node firstNode = new Node(x);
+        firstNode.item = x;
+        this.sentinel.next.prev = firstNode;
+        firstNode.next = this.sentinel.next;
+        this.sentinel.next = firstNode;
+        firstNode.prev = this.sentinel;
     }
 
     @Override
     public void addLast(T x) {
-
+        Node lastNode = new Node(x);
+        lastNode.item = x;
+        this.sentinel.prev.next = lastNode;
+        lastNode.prev = this.sentinel.prev;
+        lastNode.next = this.sentinel;
+        this.sentinel.prev = lastNode;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> returnList = new ArrayList<>();
+        Node node = this.sentinel.next;
+        while (!node.equals(sentinel)){
+            returnList.add(node.item);
+            node = node.next;
+        }
+        return returnList;
     }
 
     @Override
